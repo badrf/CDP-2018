d24696092a45c55be086094d6276b3431635894c
badrf <s.s.f.b@hotmail.fr>
2018-11-20 23:33:25 +0100

    affichage les pages web

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,25 @@ var path    = require("path");
 
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile( path.resolve('../html/index.html') );
 });
+
+app.get('/new_issue',function(req,res){
+res.sendFile( path.resolve('../html/new_issue.html') );
+});
+
+app.get('/new_project',function(req,res){
+  res.sendFile( path.resolve('../html/new_project.html') );
+});
+
+app.get('/edit_issue',function(req,res){
+  res.sendFile( path.resolve('../html/edit_issue.html') );
+});
+
+app.get('/login',function(req,res){
+  res.sendFile( path.resolve('../html/login.html') );
+});
+
 app.listen(8080);
 
 console.log("Running at Port 8080");
