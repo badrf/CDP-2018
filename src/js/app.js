597095e7cf2f893a8d51c38e151ae9e4d3cf4d61
pierrinator <plorson3@gmail.com>
2018-12-03 14:40:58 +0100

    POST fonctionnels pour addissue, addproj, addtask, addsprint, login, signup

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 var express = require("express");
-var app     = express();
-var path    = require("path");
+var bodyParser = require('body-parser')
+var app = express()
+var path = require("path");
+var jsdom = require("jsdom");
+var { JSDOM } = jsdom;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 app.get('/',function(req,res){
@@ -22,6 +28,57 @@ app.get('/edit_issue',function(req,res){
 app.get('/login',function(req,res){
   res.sendFile( path.resolve('../html/login.html') );
 });
+
+app.post('/login', function(request, response){
+  var user = request.body.username;
+  var pass = request.body.password;
+});
+
+app.post('/signup', function(request, response){
+  var user = request.body.username;
+  var pass = request.body.password;
+});
+
+app.post('/addissue', function(request, response){
+  document = new JSDOM("../src/new_issue.html").window.document;
+  var id = request.body.id;
+  var description = request.body.description;
+  var priority = request.body.priority;
+  var difficulty = request.body.difficulty;
+  var sprint = request.body.sprint;
+  console.log(id + description + priority + difficulty + sprint);
+  document.writeln("test");
+});
+
+app.post('/addproj', function(request, response){
+  var pid = request.body.pid;
+  var title = request.body.title;
+  var username = request.body.username;
+  console.log(pid + title + username);  
+});
+
+app.post('/addtask', function(request, response){
+  var id = request.body.id;
+  var description = request.body.description;
+  var composant = request.body.composant;
+  var ressource = request.body.ressource;
+  var us = request.body.us;
+  var dependency = request.body.dependency;
+  var state = request.body.state;
+  var dev = request.body.dev;
+  var jh = request.body.jh;
+  console.log(id + description + composant + ressource + us + dependency + state + dev + jh); 
+}); 
+
+app.post('/addsprint', function(request, response){
+  var start = request.body.start;
+  var end = request.body.end;
+  var state = request.body.state;
+  var number = request.body.number;
+  console.log(start + end + state + number); 
+}); 
+
+
 
 app.listen(8080);
 
