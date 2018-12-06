ab622a6395439fbfe5e59beda73d49aa2e0d3868
pierrinator <plorson3@gmail.com>
2018-12-06 12:02:06 +0100

    Affichage des entrées fonctionnel

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ var app = express()
 var path = require("path");
 var jsdom = require("jsdom");
 var { JSDOM } = jsdom;
+var none = "None";
 
 app.set('view engine', 'ejs');
 
@@ -61,16 +62,18 @@ app.post('/addissue', function(req, res){
   var sprint = req.body.sprint;
   console.log(pid + id + description + priority + difficulty + sprint);
   document.writeln("test");
-  res.render( path.resolve('../ejs/index.ejs') );  });
+  res.render( path.resolve('../ejs/new_issue.ejs'), {pid : pid, id : id, description : description, priority : priority, difficulty : difficulty, sprint : sprint}); 
+});
 
 app.post('/addproj', function(req, res){
   var pid = req.body.pid;
   var title = req.body.title;
   var username = req.body.username;
-  console.log(pid + title + username);  
-  res.render( path.resolve('../ejs/index.ejs') );});
+  res.render( path.resolve('../ejs/new_project.ejs'), { pid : pid, title : title, username : username } );
+});
 
 app.post('/addtask', function(req, res){
+  var pid = req.body.pid
   var id = req.body.id;
   var description = req.body.description;
   var component = req.body.component;
@@ -81,7 +84,8 @@ app.post('/addtask', function(req, res){
   var dev = req.body.dev;
   var jh = req.body.jh;
   console.log(id + description + component + ressource + us + dependency + state + dev + jh);
-  res.render( path.resolve('../ejs/index.ejs') );}); 
+  res.render( path.resolve('../ejs/new_task.ejs'), {pid : pid, id : id, description : description, component : component, ressource : ressource, us : us, dependency : dependency, state : state, dev : dev, jh : jh})
+}); 
 
 app.post('/addsprint', function(req, res){
   var pid = req.body.pid;
@@ -89,8 +93,8 @@ app.post('/addsprint', function(req, res){
   var end = req.body.end;
   var state = req.body.state;
   var number = req.body.number;
-  console.log(start + end + state + number);
-  res.render( path.resolve('../ejs/index.ejs') );}); 
+  res.render( path.resolve('../ejs/new_sprint.ejs'), {pid : pid, start : start, end : end, state : state, number : number})
+}); 
 
 
 
